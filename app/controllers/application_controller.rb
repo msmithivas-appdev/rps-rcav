6b55f984298f8b53f6b517feebed9f5e9d4b8ed8
msmithivas <msmithivas@gmail.com>
2023-02-21 21:01:25 +0000

    rock paper scissors first pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,24 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # redirect_to("https://www.wikipedia.org")
+    # render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    render({ :template => "game_templates/user_rock.html.erb" })
+
+  end
+
+  def play_paper
+   
+    render({ :template => "game_templates/user_paper.html.erb" })
+
+  end
+
+  def play_scissors
+   
+    render({ :template => "game_templates/user_scissors.html.erb" })
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "application", :action => "play_rock"})
+  get("/", { :controller => "application", :action => "homepage" })
+  get("/", { :controller => "application", :action => "homepage" })
+  get("/paper", { :controller => "application", :action => "play_paper"})
+  get("/scissors", { :controller => "application", :action => "play_scissors"})
 
 
 end
